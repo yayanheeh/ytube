dad94128847e29b76a7267c9a54a69880734dfa6
yayanheeh <tokoxp58@gmail.com>
2019-06-28 11:28:58 +0700

    Update Header.vue

--- src/app/Header/Header.vue
+++ src/app/Header/Header.vue
@@ -9,7 +9,7 @@
     app
   >
     <v-toolbar-side-icon @click="toggleDrawer"/>
-    Hoye
+    <b>Yplay</b>
 
     <v-layout 
       row 
@@ -50,69 +50,7 @@
   </v-toolbar>
   
   
-  <!-- mobile hiden -->
-  
-  
-  <v-row class="hidden-sm-and-up">
-   <v-toolbar
-    color="vTyoobRed"
-    dense
-    fixed
-    dark
-    clipped-left
-    :height="64"
-    app
-  >
-    <v-toolbar-side-icon @click="toggleDrawer"/>
-    <router-link 
-      @click.native="sendLogoClickGA"
-      aria-label="home" 
-      :to="{name: 'home'}">
-      <img 
-        class="logo mr-2" 
-        alt="Youtube Logo"
-        src="/img/full-logo-white.png">
-    </router-link>
-
-    <v-layout 
-      row 
-      justify-center>
-      <v-flex 
-        xs12 
-        md8 
-        lg6 >
-        <v-form 
-          @submit.prevent="searchVideos">
-          <v-combobox
-            ref="searchBox"
-            :menu-props="{
-              transition: 'slide-y-transition'
-            }"
-            clearable
-            class="mx-3"
-            no-filter
-            :loading="searching"
-            :items="searchSuggestions"
-            :append-icon="null"
-            :search-input.sync="query"
-            v-model="searchSelectedValue"
-            @input="searchVideos"
-            dark
-            flat
-            full-width
-            prepend-inner-icon="search"
-            hide-no-data
-            hide-details
-            :label="$t('searchHeaderPlaceholder')"
-            solo-inverted
-          />
-        </v-form>
-      </v-flex>
-    </v-layout>
-
-  </v-toolbar>
-  </v-row>
-  
+ 
 </template>
 
 <style scoped>
@@ -121,15 +59,7 @@
   margin-top: 8px;
 }
 </style>
-<script>
-new Vue({
-  el: '#app',
-  data () {
-    return {
-    }
-  }
-})
-</script>
+
 
 <script>
 import Header from "./Header.ts";
